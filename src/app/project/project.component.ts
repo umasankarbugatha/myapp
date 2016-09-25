f743a97b10bb6a1cd4a29b2fdbf677547fcbeb11
H179540 <umasankararao.bugatha@halliburton.com>
2016-09-25 01:16:49 -0500

    my app first push

---
+++ src/app/project/project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oec-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
